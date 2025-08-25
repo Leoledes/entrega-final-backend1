1af9664247b9136ad1c56ab452e7104cb0bd453d
Leoledes <ledesma.leonardoa@gmail.com>
2025-08-24 22:18:14 -0300

    Configuración de directorios

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const app = express();
+import fs from "fs/promises";
+import crypto from "crypto";
 
-const fs = require("fs/promises");
 
-const PORT = 8080;
 
 app.use(express.json());
+const PORT = 8080;
+
+const DATA_DIR = "./db";
+const PLANTS_FILE = `${DATA_DIR}/plants.json`;
+const initializeFiles = async () => {
+  try {
+    await fs.mkdir(DATA_DIR, { recursive: true });
+    await fs.access(PLANTS_FILE).catch(async () => {
+      await fs.writeFile(PLANTS_FILE, "[]", "utf-8");
+      console.log("Archivo plants.json creado para almacenar las plantas.");
+    });
+  } catch (error) {
+    console.error("Error al inicializar los archivos:", error);
+  }
+};
+
+const fs = require("fs/promises");
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
