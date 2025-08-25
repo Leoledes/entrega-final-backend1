b59d86852dbef7cca8b96b0cbc5311bd2fede341
Leoledes <ledesma.leonardoa@gmail.com>
2025-08-25 18:00:49 -0300

    Configurado .env

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+require('dotenv').config();
 const PORT = process.env.PORT || 8080;
 const { readJson, writeJson } = require("./src/utils/fileManager");
 const ProductManager = require("./src/managers/productManager");
