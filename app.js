30d5fc971f90cdcf73fd83d4e7b03b16853036c9
Leoledes <ledesma.leonardoa@gmail.com>
2025-08-24 21:00:03 -0300

    Agregados DB y MW

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 const express = require("express");
 const app = express();
 
+const fs = require("fs/promises");
+
 const PORT = 8080;
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Servidor funcionando");
+});
+
+app.get("/api/products", (req, res) => {
+  const data = fs.readFileSync("./db/products.json", "utf-8");
+  const products = JSON.parse(data || "[]");
+  res.json(products);
 });
