fac4aa3989a80e033f222b1609c35496c666eebd
Leoledes <ledesma.leonardoa@gmail.com>
2025-09-10 11:41:04 -0300

    Modularización controllers

---
+++ src/controllers/products.controllers.js
@@ -0,0 +1,73 @@
+// Controlador para manejar la lógica de las rutas de productos
+const productService = require('../services/products.service');
+
+const getProducts = async (req, res) => {
+    try {
+        const products = await productService.getAllProducts();
+        res.json(products);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+const getProductById = async (req, res) => {
+    try {
+        const { pid } = req.params;
+        const product = await productService.getProductById(pid);
+        if (!product) {
+            return res.status(404).json({ error: "Producto no encontrado" });
+        }
+        res.json(product);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+const addProduct = async (req, res) => {
+    try {
+        const newProductData = req.body;
+        // Validar los datos de entrada
+        if (!newProductData.title || !newProductData.description || !newProductData.code || !newProductData.price || typeof newProductData.status !== 'boolean' || !newProductData.stock || !newProductData.category || !newProductData.thumbnails) {
+            return res.status(400).json({ error: "Faltan campos obligatorios o el formato es incorrecto" });
+        }
+        const newProduct = await productService.addNewProduct(newProductData);
+        res.status(201).json(newProduct);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+const updateProduct = async (req, res) => {
+    try {
+        const { pid } = req.params;
+        const updateData = req.body;
+        const updatedProduct = await productService.updateProduct(pid, updateData);
+        if (!updatedProduct) {
+            return res.status(404).json({ error: "Producto no encontrado" });
+        }
+        res.json(updatedProduct);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+const deleteProduct = async (req, res) => {
+    try {
+        const { pid } = req.params;
+        const success = await productService.deleteProduct(pid);
+        if (!success) {
+            return res.status(404).json({ error: "Producto no encontrado" });
+        }
+        res.json({ message: "Producto eliminado exitosamente" });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+module.exports = {
+    getProducts,
+    getProductById,
+    addProduct,
+    updateProduct,
+    deleteProduct
+};
