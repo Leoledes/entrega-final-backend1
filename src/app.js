9c69ab1f08a8736c415615e9576f2bfae1b86f63
Leoledes <ledesma.leonardoa@gmail.com>
2025-09-26 13:11:49 -0300

    Cambios en estructura 6

--- src/app.js
+++ src/app.js
@@ -1,43 +1,36 @@
 const express = require('express');
-const app = express();
 const handlebars = require('express-handlebars');
 
 const productsRouter = require('./routes/products.routes');
-const cartsRouter = require('./routes/carts.routes');
 const viewsRouter = require('./routes/views.router');
+
+const app = express();
 
 app.engine('handlebars', handlebars.engine());
 app.set('views', __dirname + '/views');
 app.set('view engine', 'handlebars');
 
-app.use(express.static(__dirname + '/../public'));
-
+app.use(express.static(__dirname + '/../../public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/', viewsRouter);
 app.use('/api/products', productsRouter);
-app.use('/api/carts', cartsRouter);
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  if (req.method === "OPTIONS") return res.sendStatus(200);
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    if (req.method === "OPTIONS") return res.sendStatus(200);
+    next();
 });
 
-app.get("/test", (req, res) => {
-  res.send("Servidor funcionando correctamente");
+app.get("/", (req, res) => {
+    res.send("Servidor funcionando");
 });
 
 app.use((req, res) => {
-  res.status(404).json({ status: "error", message: "Ruta no encontrada" });
-});
-
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).json({ status: "error", message: err.message });
+    res.status(404).json({ status: "error", message: "Ruta no encontrada" });
 });
 
 module.exports = app;
